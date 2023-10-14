35de2243fafdb9c3394e93ad9063dc2d837b9940
Andrii <andrii@gmail.com>
2023-10-15 00:27:37 +0300

    CHANGE bff variable

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://090236rfvg.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://x01htwz1v5.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
