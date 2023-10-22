17f8c127633608ca13e0b064eeb4a2348b3b5a06
Andrii <andrii@gmail.com>
2023-10-22 21:46:50 +0300

    ADD ability to import file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://f0b8pndc5m.execute-api.eu-west-1.amazonaws.com",
   bff: "https://x01htwz1v5.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
