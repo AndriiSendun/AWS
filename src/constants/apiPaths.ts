3b5eef7021f410a1310066c55e343f9449628ba1
Andrii <andrii@gmail.com>
2023-10-29 21:22:10 +0200

    change bff url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://f0b8pndc5m.execute-api.eu-west-1.amazonaws.com",
-  bff: "https://x01htwz1v5.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://qrj6k2g52b.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
